3ff1a460bb985706e7bbb19371db5c67c67867df
Liang Liu <liangliu@fnal.gov>
2024-04-02 14:11:12 -0500

    Update to uboonecode v80_00_00_78

--- config/setup.sh
+++ config/setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-source /grid/fermiapp/products/uboone/setup_uboone.sh
+#source /grid/fermiapp/products/uboone/setup_uboone.sh
+source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
 echo "Setting up uboonecode"
-setup uboonecode v07_13_00 -q e17:prof
-#setup uboonecode v07_05_00_02 -q e17:prof
-echo "Setting up larsoftobj"
-setup larsoftobj v07_08_00 -q e17:prof
-source /uboone/app/users/cadams/pystack2/setup.sh
+setup uboonecode v08_00_00_78 -q e17:prof
+
+#echo "Setting up larsoftobj"
+#setup larsoftobj v07_08_00 -q e17:prof
+source /exp/uboone/app/users/cadams/pystack2/setup.sh
 
 # # clean up previously set env
 # me="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
